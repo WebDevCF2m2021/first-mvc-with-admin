32c378f7d84a277648d15c808e5d40fdbaa7143b
mikhawa <michaeljpitz@gmail.com>
2021-11-23 15:32:36 +0100

    sql homepage with 250 char

--- model/thearticleModel.php
+++ model/thearticleModel.php
@@ -5,18 +5,19 @@
 // affichage de tous les articles sur la page d'accueil (tant qu'ils ont un auteur)
 function thearticleHomepageSelectAll(mysqli $db): array{
     // requête
-    $sql="SELECT thearticle.*, 
-    theuser.theuserName, theuser.theuserLogin,	
-                 GROUP_CONCAT(thesection.idthesection) AS idthesection, 
-                 GROUP_CONCAT(thesection.thesectionTitle SEPARATOR '|||') AS thesectionTitle
-FROM thearticle
- INNER JOIN theuser
-     ON thearticle.theuser_idtheuser = theuser.idtheuser 
- LEFT JOIN thearticle_has_thesection
-     ON thearticle.idthearticle = thearticle_has_thesection.thearticle_idthearticle
- LEFT JOIN thesection
-     ON thearticle_has_thesection.thesection_idthesection = thesection.idthesection 
- GROUP BY thearticle.idthearticle     ;";
+    $sql="SELECT a.idthearticle, a.thearticleTitle, LEFT(a.thearticleText,250) AS thearticleText, a.thearticleDate,
+    u.idtheuser, u.theuserName, u.theuserLogin,	
+                  GROUP_CONCAT(s.idthesection) AS idthesection, 
+                  GROUP_CONCAT(s.thesectionTitle SEPARATOR '|||') AS thesectionTitle
+FROM thearticle a
+  INNER JOIN theuser u
+      ON a.theuser_idtheuser = u.idtheuser 
+  LEFT JOIN thearticle_has_thesection h
+      ON a.idthearticle = h.thearticle_idthearticle
+  LEFT JOIN thesection s
+      ON h.thesection_idthesection = s.idthesection
+WHERE a.thearticleStatus = 1
+  GROUP BY a.idthearticle    ;";
 
 // récupération des articles, ou affichage de l'erreur SQL et arrêt
 $recup = mysqli_query($db,$sql) or die("Erreur SQL :".mysqli_error($db));
