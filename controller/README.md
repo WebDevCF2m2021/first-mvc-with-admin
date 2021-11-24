45e5658fa64953e65bc44b15c8015ab8cb3d1169
SashaKlash <sashaderuyver@gmail.com>
2021-11-24 16:17:46 +0100

    modif de config.php et relecture di fichier.

---
+++ controller/README.md
@@ -0,0 +1,5 @@
+# controller
+
+C'est le dossier qui contiendra les controllers permettant le lien entre les actions utilisateur ("routages"), les modèles et les vues
+
+Il s'agit d'une extension de `public/index.php` qui lui-même est un contrôleur dit frontal
---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ chargement des dépendances
+*/
+
+// chargement de la configuration
+require_once "../config.php";
+// chargement du modèle thearticle (ici dans le CF car utilisé partout)
+require_once "../model/thearticleModel.php";
+
+// connexion à la DB
+$dbConnect = mysqli_connect(DB_HOST,DB_LOGIN,DB_PWD,DB_NAME,DB_PORT);
+mysqli_set_charset($dbConnect,DB_ENCODE);
+
+// pour le moment on est sur accueil
+$recupArticle = thearticleHomepageSelectAll($dbConnect);
+
+/*
+ICI
+*/
+
+var_dump($recupArticle);
+
+// chargement de la vue de homepage
+include_once "../view/publicView/homepageView.php";
