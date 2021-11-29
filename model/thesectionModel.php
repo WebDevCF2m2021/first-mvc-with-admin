a674f02ca9df63c4d115d7e8eda5fb7b389add65
mikhawa <michaeljpitz@gmail.com>
2021-11-29 15:49:41 +0100

    thesectionSelectOne

--- model/thesectionModel.php
+++ model/thesectionModel.php
@@ -2,10 +2,22 @@
 
 // fonction qui récupère les sections classées par thesectionTitle ASC, sans thesectionDesc (menu + insert et update)
 
-function thesectionSelectAll(mysqli $db): array {
+function thesectionSelectAll(mysqli $db): array
+{
     $sql = "SELECT idthesection, thesectionTitle
             FROM thesection
             ORDER BY thesectionTitle ASC;";
-    $request = mysqli_query($db,$sql) or die("Erreur sql : ".mysqli_error($db));
-    return mysqli_fetch_all($request,MYSQLI_ASSOC);
+    $request = mysqli_query($db, $sql) or die("Erreur sql : " . mysqli_error($db));
+    return mysqli_fetch_all($request, MYSQLI_ASSOC);
+}
+
+// fonction qui récupère une section grâce son id
+
+function thesectionSelectOne(mysqli $db, int $id): array
+{
+    $sql = "SELECT idthesection, thesectionTitle, thesectionDesc
+            FROM thesection
+            WHERE idthesection = $id";
+    $request = mysqli_query($db, $sql) or die("Erreur sql : " . mysqli_error($db));
+    return mysqli_fetch_assoc($request);
 }
