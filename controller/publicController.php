6951fbfa6be1d872812d6c0464e5a944d15717a0
mikhawa <michaeljpitz@gmail.com>
2021-11-29 15:43:01 +0100

    router ok

--- controller/publicController.php
+++ controller/publicController.php
@@ -1,18 +1,26 @@
 <?php
 
 
-// si on a cliqué sur une section
-if (isset($_GET['idsection'])) {
-    echo "id de la section : " . $_GET['idsection'];
-    // si on a cliqué sur le détail d'un article
-} elseif (isset($_GET['idarticle'])) {
+// si on a cliqué sur une section et que sa valeur est de type texte ne contenant QUE des numériques (ctype_digit) et pas le 0 (!empty => différent de vide)
+if (isset($_GET['idsection']) && ctype_digit($_GET['idsection']) && !empty($_GET['idsection'])) {
+    // conversion en integer
+    $idsection = (int) $_GET['idsection'];
+    echo "id de la section : " . $idsection;
 
-    echo "id de l'article : " . $_GET['idarticle'];
+    // si on a cliqué sur le détail d'un article
+} elseif (isset($_GET['idarticle']) && ctype_digit($_GET['idarticle']) && !empty($_GET['idarticle'])) {
+
+    // conversion en integer
+    $idarticle = (int) $_GET['idarticle'];
+    echo "id de l'article : " . $idarticle;
 
     // si on a cliqué sur le détail d'un utilisateur
-} elseif (isset($_GET['iduser'])) {
+} elseif (isset($_GET['iduser']) && ctype_digit($_GET['iduser']) && !empty($_GET['iduser'])) {
 
-    echo "id de l'utilisateur : " . $_GET['iduser'];
+    // conversion en integer
+    $iduser = (int) $_GET['iduser'];
+    echo "id de l'utilisateur : " . $iduser;
+
     // sinon on est sur l'accueil
 } else {
 
