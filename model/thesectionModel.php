5cbfe1caad2792eebe68addd0b08fabb2d7069d5
mikhawa <michaeljpitz@gmail.com>
2021-11-29 16:29:04 +0100

    section by id - to be continued

--- model/thesectionModel.php
+++ model/thesectionModel.php
@@ -11,13 +11,15 @@ function thesectionSelectAll(mysqli $db): array
     return mysqli_fetch_all($request, MYSQLI_ASSOC);
 }
 
-// fonction qui récupère une section grâce son id
+// fonction qui récupère une section grâce son id, arguments obligatoires ($db en mysqli, $id en int) et retour obligatoir un tableau (array) ou en PHP 7 un tableau ou null (?array)
 
-function thesectionSelectOne(mysqli $db, int $id): array
+function thesectionSelectOne(mysqli $db, int $id): ?array
 {
     $sql = "SELECT idthesection, thesectionTitle, thesectionDesc
             FROM thesection
             WHERE idthesection = $id";
     $request = mysqli_query($db, $sql) or die("Erreur sql : " . mysqli_error($db));
+
+    // si on a un résultat (la section existe) envoi d'un tableau associatif ou null si il est vide
     return mysqli_fetch_assoc($request);
 }
