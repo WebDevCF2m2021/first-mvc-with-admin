6f210630bfa16cd99d54bff47ed9b513af57f88b
SashaKlash <sashaderuyver@gmail.com>
2021-12-01 09:43:25 +0100

    màj

--- public/index.php
+++ public/index.php
@@ -1,12 +1,7 @@
+<?php
+
 // chargement des dépendances
 require_once "../config.php";
 
-<<<<<<< HEAD
-<?php
-
-   echo "Hello World !";
-?>
-=======
 // chargement de la vue de homepage
 include_once "../view/publicView/homepageView.php";
->>>>>>> 74050f98b8be328990cb7c90dd9623d190099805
