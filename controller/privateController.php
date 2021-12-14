996fa375b5f0cb2882ccb48e35e7f737a845a515
mikhawa <michaeljpitz@gmail.com>
2021-12-14 15:55:25 +0100

    disconnect ok

--- controller/privateController.php
+++ controller/privateController.php
@@ -1,3 +1,28 @@
 <?php
 
-var_dump($_SESSION);
+
+// si on a cliqué sur déconnexion, ou que la session n'est plus valide, on force la déconnexion
+if (isset($_GET['disconnect']) || $_SESSION['myID'] != session_id()) {
+    theuserDisconnect();
+    header("Location: ./");
+    exit();
+}
+
+// on va charger des contrôleurs différents suivant les permissions
+switch ($_SESSION['therightPerm']) {
+        // admin
+    case 3:
+        // chargement du contrôleur de l'admin
+        require_once "private/adminController.php";
+        break;
+        // moderator
+    case 2:
+
+        break;
+        // redactor    
+    case 1:
+
+        break;
+        // user (0)
+    default:
+}
