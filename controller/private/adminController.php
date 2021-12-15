293e145ecd6396a24d73d1f8bb9b2ef92f5a05ed
QuentinFayt <quentin.fayt.p@gmail.com>
2021-12-15 15:31:58 +0100

    homepageArticlesAdminView

--- controller/private/adminController.php
+++ controller/private/adminController.php
@@ -1,5 +1,18 @@
 <?php
-var_dump($_SESSION);
+
+if (isset($_GET["p"])) {
+    if ($_GET["p"] == "article") {
+        if (isset($_GET["create"])) {
+        } else {
+            $recupArticles = thearticleAdminSelectAll($dbConnect);
+            require_once "../view/adminView/homepageArticlesAdminView.php";
+        }
+    } elseif ($_GET["p"] == "user") {
+    } elseif ($_GET["p"] == "section") {
+    }
+} else {
+    require_once "../view/adminView/homepageAdminView.php";
+}
 /*
 ?p=homepage
 ?p=article
@@ -30,32 +43,3 @@ var_dump($_SESSION);
 ?disconnect
 ./ => homepage
 */
-
-echo "<pre>?p=homepage
-?p=article
-    &create
-        - choix de l'auteur
-        - choix des sections
-        - choix de l'affichage ou non
-    &update={id}
-        - choix de l'auteur
-        - choix des sections
-        - choix de l'affichage ou non
-        - choix de la date
-    &delete={id}
-        - tous les articles, mais avec confirmation
-?p=user
-    &create
-        - tout
-        - choix du droit
-    &update={id}
-        - tout
-        - choix du droit
-    &delete={id}
-        - Tous sauf lui-même
-?p=section
-    &create
-    &update={id}
-    &delete={id}
-?disconnect
-./ => homepage</pre>";
--- model/thearticleModel.php
+++ model/thearticleModel.php
@@ -129,7 +129,28 @@ WHERE a.thearticleStatus = 1 AND s.idthesection = $idsection
 
     return mysqli_fetch_all($recup, MYSQLI_ASSOC);
 }
+function thearticleAdminSelectAll(mysqli $db): array
+{
+    // requête
+    $sql = " SELECT a.idthearticle, a.thearticleTitle, a.thearticleStatus, LEFT(a.thearticleText,400) AS thearticleText, a.thearticleDate,
+    u.idtheuser, u.theuserName, u.theuserLogin,	
+                  GROUP_CONCAT(s.idthesection) AS idthesection, 
+                  GROUP_CONCAT(s.thesectionTitle SEPARATOR '|||') AS thesectionTitle
+FROM thearticle a
+  LEFT JOIN theuser u
+      ON a.theuser_idtheuser = u.idtheuser 
+  LEFT JOIN thearticle_has_thesection h
+      ON a.idthearticle = h.thearticle_idthearticle
+  LEFT JOIN thesection s
+      ON h.thesection_idthesection = s.idthesection
+  GROUP BY a.idthearticle
+  ORDER BY a.thearticleDate DESC;  ";
 
+    // récupération des articles, ou affichage de l'erreur SQL et arrêt
+    $recup = mysqli_query($db, $sql) or die("Erreur SQL :" . mysqli_error($db));
+
+    return mysqli_fetch_all($recup, MYSQLI_ASSOC);
+}
 /**
  * cuteTheText
  *
