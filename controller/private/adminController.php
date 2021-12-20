890fded1e7ce216c3b9426b833927b81f836dd1f
QuentinFayt <quentin.fayt.p@gmail.com>
2021-12-20 15:31:25 +0100

    comment

--- controller/private/adminController.php
+++ controller/private/adminController.php
@@ -6,7 +6,7 @@ if (isset($_GET['p'])) {
     // si on gère les articles
     if ($_GET['p'] == "article") {
 
-        // si on veut créer un nouvel article (Crud)
+        // si on veut créer un nouvel article
         if (isset($_GET['create'])) {
 
             // si on poste le formulaire
@@ -19,23 +19,16 @@ if (isset($_GET['p'])) {
                 $texte = htmlspecialchars(strip_tags(trim($_POST['thearticleText'])), ENT_QUOTES);
                 $status = (int) $_POST['thearticleStatus'];
                 $iduser = (int) $_POST['theuser_idtheuser'];
-                // si on a au moins une section il sera au format tableau indexé
                 $sections = (isset($_POST['idthesection']) && is_array($_POST['idthesection']))
                     ?   $_POST['idthesection']
-                    // sinon on lui passe un tableau vide
                     : [];
-                $insert = thearticleInsertWithUserAndSection($dbConnect, $titre, $texte, $status, $iduser, $sections);
 
+                /*
+ON EST ICI
 
-                // insertion de l'article et de ses rubriques (si existantes) dans la base de donnée
-                $insert = thearticleInsertWithUserAndSection($dbConnect, $titre, $texte, $status, $iduser, $sections);
+                    */
 
-                // si l'insertion est effectuée
-                if ($insert) {
-                    // redirection vers la gestion d'articles
-                    header("location: ./?p=article");
-                    die;
-                }
+                thearticleInsertWithUserAndSection($dbConnect, $titre, $texte, $status, $iduser, $sections);
             }
 
             // chargement de tous les auteurs disponibles
@@ -48,38 +41,11 @@ if (isset($_GET['p'])) {
             // chargement de la vue
             require_once "../view/adminView/articlesCreateAdminView.php";
 
-
             // sinon affichage de tous les articles    
-        } elseif (isset($_GET['update']) && ctype_digit($_GET["update"]) && !empty($_GET["update"])) {
-
-            // transtypage de string à int
-            $idArticle = (int) $_GET["update"];
-
-            // chargement de l'article grâce à son id
-            $article =  thearticleAdminSelectOneById($dbConnect, $idArticle);
-
-
-            if (!empty($article["idthearticle"])) {
-
-                // chargement de tous les auteurs disponibles
-                $authors = theuserSelectAll($dbConnect);
-
-                // chargement de toutes les sections
-                $sections = thesectionSelectAll($dbConnect);
-
-                // chargement de la vue
-                require_once "../view/adminView/articlesUpdateAdminView.php";
-            } else {
-                header("Location: ./?p=article");
-                exit;
-            }
-        } elseif (isset($_GET['delete']) && ctype_digit($_GET["delete"]) && !empty($_GET["delete"])) {
         } else {
 
             // appel de la fonction qui récupère tous les articles
             $recupArticles = thearticleAdminSelectAll($dbConnect);
-
-            // Pour valider ou invalider l'affichage d'un article en 1 clic
             if (isset($_POST["valid"])) {
                 $id = (int) $_POST["valid"];
                 $validation = true;
